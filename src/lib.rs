574a4898d6128c52438e6e72b541a92c17b40691
ardura <azviscarra@gmail.com>
2023-05-24 09:38:15 -0700

    Working beta!

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 use atomic_float::AtomicF32;
-//use ::egui::mutex::Mutex;
+//use ::egui::{style, Color32, Frame};
 use nih_plug::{prelude::*};
-use nih_plug_egui::{create_egui_editor, egui::{self, mutex::Mutex}, widgets, EguiState};
+use nih_plug_egui::{create_egui_editor, egui::{self, mutex::Mutex, plot::{Line, PlotPoints}, Color32, Frame, RichText}, widgets, EguiState};
 use std::{sync::{Arc, atomic::Ordering}};
-
-
 
 /**************************************************
  * Scrollscope by Ardura
@@ -12,27 +10,22 @@ use std::{sync::{Arc, atomic::Ordering}};
  * Build with: cargo xtask bundle scrollscope --profile profiling
  * ************************************************/
 
-/// The time it takes for the peak meter to decay by 12 dB after switching to complete silence.
-const PEAK_METER_DECAY_MS: f64 = 100.0;
-
 pub struct Gain {
     params: Arc<GainParams>,
 
-    // normalize the peak meter's response based on the sample rate with this
-    peak_meter_decay_weight: f32,
+    // Counter for scaling sample skipping
+    skip_counter: i32,
+    
+    toggle_ontop: Arc<Mutex<bool>>,
 
-    // Compressor class
-    //osc_obj: Oscilloscope,
-
-    // The current data for the different meters
-    peak_meter: Arc<AtomicF32>,
+    // Data holding values
     samples: Arc<Mutex<Vec<AtomicF32>>>,
+    aux_samples: Arc<Mutex<Vec<AtomicF32>>>,
 }
 
 #[derive(Params)]
 struct GainParams {
-    /// The editor state, saved together with the parameter state so the custom scaling can be
-    /// restored.
+    /// The editor state
     #[persist = "editor-state"]
     editor_state: Arc<EguiState>,
 
@@ -42,16 +35,24 @@ struct GainParams {
 
     /// Scrolling speed for GUI
     #[id = "scrollspeed"]
-    pub scrollspeed: FloatParam,
+    pub scrollspeed: IntParam,
+
+    /// Horizontal Scaling
+    #[id = "scaling"]
+    pub h_scale: IntParam,
+
+    //#[id = "toggle_ontop"]
+    //pub toggle_ontop: BoolParam,
 }
 
 impl Default for Gain {
     fn default() -> Self {
         Self {
             params: Arc::new(GainParams::default()),
-            peak_meter_decay_weight: 1.0,
-            peak_meter: Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
+            skip_counter: 0,
+            toggle_ontop: Arc::new(Mutex::new(false)),
             samples: Arc::new(Mutex::new(Vec::new())),
+            aux_samples: Arc::new(Mutex::new(Vec::new())),
         }
     }
 }
@@ -77,11 +78,20 @@ impl Default for GainParams {
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
 
             // scrollspeed parameter
-            scrollspeed: FloatParam::new(
-                "Scroll Speed",
-                5.0,
-                    FloatRange::Linear {min: 0.5, max: 30.0 },
+            scrollspeed: IntParam::new(
+                "Samples",
+                8000,
+                    IntRange::Linear {min: 5000, max: 30000 },
             ),
+
+            // scaling parameter
+            h_scale: IntParam::new(
+                "Scale",
+                20,
+                    IntRange::Linear {min: 1, max: 50 },
+            ),
+
+            //toggle_ontop: BoolParam::new("Toggle Ontop", true),
         }
     }
 }
@@ -96,8 +106,9 @@ impl Plugin for Gain {
 
     // This looks like it's flexible for running the plugin in mono or stereo
     const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[
-        AudioIOLayout {main_input_channels: NonZeroU32::new(2), main_output_channels: NonZeroU32::new(2), ..AudioIOLayout::const_default()},
-        AudioIOLayout {main_input_channels: NonZeroU32::new(1), main_output_channels: NonZeroU32::new(1), ..AudioIOLayout::const_default()},
+        //              Inputs                                      Outputs                                 sidechain                               No Idea but needed
+        AudioIOLayout {main_input_channels: NonZeroU32::new(2), main_output_channels: NonZeroU32::new(2), aux_input_ports: &[new_nonzero_u32(2)], ..AudioIOLayout::const_default()},
+        AudioIOLayout {main_input_channels: NonZeroU32::new(1), main_output_channels: NonZeroU32::new(1), aux_input_ports: &[new_nonzero_u32(1)], ..AudioIOLayout::const_default()},
     ];
 
     const SAMPLE_ACCURATE_AUTOMATION: bool = true;
@@ -111,52 +122,128 @@ impl Plugin for Gain {
 
     fn editor(&self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
         let params = self.params.clone();
-        let peak_meter = self.peak_meter.clone();
         let samples = self.samples.clone();
+        let aux_samples = self.aux_samples.clone();
+        let ontop = self.toggle_ontop.clone();
         create_egui_editor(
             self.params.editor_state.clone(),
             (),
             |_, _| {},
             move |egui_ctx, setter, _state| {
-                egui::CentralPanel::default().show(egui_ctx, |ui| {
-                    // NOTE: See `plugins/diopser/src/editor.rs` for an example using the generic UI widget
+                egui::CentralPanel::default()
+                    .frame(Frame::none().fill(Color32::from_rgb(10,10,10)))
+                    .show(egui_ctx, |ui| {
 
-                    ui.horizontal(|ui| {
-                        ui.label("Gain");
-                        ui.add(widgets::ParamSlider::for_param(&params.free_gain, setter));
+                        // Defined colors
+                        const ORANGE: Color32 = Color32::from_rgb(239,123,69);
+                        const CYAN: Color32 = Color32::from_rgb(14,177,210);
+                        const DARK: Color32 = Color32::from_rgb(10, 10, 10);
+                        const GREY: Color32 = Color32::from_rgb(20, 20, 20);
+                        let primay_line_color: Color32 = ORANGE;
+                        let aux_line_color: Color32 = CYAN;
 
-                        let peak_meter =
-                            util::gain_to_db(peak_meter.load(std::sync::atomic::Ordering::Relaxed));
-                        let peak_meter_text = if peak_meter > util::MINUS_INFINITY_DB {
-                                format!("{peak_meter:.1} dBFS")
-                            } else {
-                                String::from("-inf dBFS")
-                            };
+                        // Change colors - there's probably a better way to do this
+                        let mut style_var = ui.style_mut().clone();
+                        style_var.visuals.widgets.inactive.fg_stroke.color = ORANGE;
+                        style_var.visuals.widgets.noninteractive.fg_stroke.color = ORANGE;
+                        style_var.visuals.widgets.noninteractive.bg_stroke.color = CYAN;
+                        style_var.visuals.widgets.noninteractive.bg_fill = DARK;
+                        style_var.visuals.widgets.inactive.bg_fill = GREY;
+                        style_var.visuals.widgets.inactive.bg_stroke.color = ORANGE;
+                        style_var.visuals.widgets.active.fg_stroke.color = ORANGE;
+                        style_var.visuals.widgets.active.bg_stroke.color = ORANGE;
+                        style_var.visuals.widgets.open.fg_stroke.color = ORANGE;
 
-                            let peak_meter_normalized = (peak_meter + 60.0) / 60.0;
-                            ui.allocate_space(egui::Vec2::splat(2.0));
-                            ui.add(
-                                egui::widgets::ProgressBar::new(peak_meter_normalized)
-                                    .text(peak_meter_text),
-                            );
-                    });
-                    
-                    ui.horizontal(|ui| {
-                        // Oscilloscope code
-                        // https://github.com/emilk/egui/blob/master/crates/egui_demo_lib/src/demo/painting.rs?
-                        
-                        for i in 1..samples.lock().len() {
-                            let prev_sample = &samples.lock()[i - 1];
-                            let curr_sample = &samples.lock()[i];
-                
-                            let prev_pos = egui::pos2(i as f32 - 1.0, prev_sample.load(Ordering::Relaxed));
-                            let curr_pos = egui::pos2(i as f32, curr_sample.load(Ordering::Relaxed));
-                
-                            let line_color = egui::Color32::GREEN;
-                            let line_stroke = egui::Stroke::new(1.0, line_color);
-                            ui.painter().line_segment([prev_pos, curr_pos], line_stroke);
-                        }
-                    });
+                        // Rects around params
+                        //style_var.visuals.widgets.active.bg_fill = CYAN;
+                        // Param fill
+                        style_var.visuals.selection.bg_fill = ORANGE;
+
+                        ui.set_style(style_var);
+
+                        ui.horizontal(|ui| {
+                            ui.add_space(6.0);
+                            ui.label(RichText::new("Scrollscope"));
+                            ui.add_space(6.0);
+
+                            ui.label("Gain");
+                            ui.add(widgets::ParamSlider::for_param(&params.free_gain, setter));
+
+                            ui.add_space(6.0);
+
+                            ui.label("Samples");
+                            ui.add(widgets::ParamSlider::for_param(&params.scrollspeed, setter));
+
+                            ui.add_space(6.0);
+
+                            ui.label("Scale");
+                            ui.add(widgets::ParamSlider::for_param(&params.h_scale, setter));
+
+                            ui.add_space(6.0);
+                            ui.checkbox(&mut ontop.lock(), "Order").on_hover_text("Change the drawing order of waveforms");
+                        });
+
+                        ui.allocate_ui(egui::Vec2::splat(100.0), |ui| {
+                            let samples = samples.lock();
+                            let aux_samples = aux_samples.lock();
+
+                            // Primary Input
+                            let data: PlotPoints = samples
+                                .iter()
+                                .enumerate()
+                                .map(|(i, sample)| {
+                                    //let h_scale = params.h_scale.value() as f64;
+                                    //if i as f64 % h_scale == 0.0 {
+                                        let x = i as f64;
+                                        let y = sample.load(Ordering::Relaxed) as f64;
+                                        [x, y]
+                                    //} else {
+                                    //    None
+                                    //}
+                                })
+                                .collect();
+                            let line = Line::new(data).color(primay_line_color);
+
+                            // Aux input
+                            let aux_data: PlotPoints = aux_samples
+                                .iter()
+                                .enumerate()
+                                .map(|(i, sample)| {
+                                    //let h_scale = params.h_scale.value() as f64;
+                                    //if i as f64 % h_scale == 0.0 {
+                                        let x = i as f64;
+                                        let y = sample.load(Ordering::Relaxed) as f64;
+                                        [x, y]
+                                    //} else {
+                                    //    None
+                                    //}
+                                })
+                                .collect();
+                            let aux_line = Line::new(aux_data).color(aux_line_color);
+
+                            egui::plot::Plot::new("Oscilloscope")
+                            .show_background(false)
+                            .include_x(400.0)
+                            .include_y(-1.0)
+                            .include_y(1.0)
+                            .center_y_axis(true)
+                            .allow_zoom(false)
+                            .allow_scroll(true)
+                            .height(310.0)
+                            .width(835.0)
+                            .allow_drag(false)
+                            .show(ui, |plot_ui| {
+                                if *ontop.lock() {
+                                    plot_ui.line(line);
+                                    plot_ui.line(aux_line);
+                                }
+                                else {
+                                    plot_ui.line(aux_line);
+                                    plot_ui.line(line);
+                                }
+                            })
+                            .response;
+                        });
                 });
             },
         )
@@ -165,64 +252,75 @@ impl Plugin for Gain {
     fn initialize(
         &mut self,
         _audio_io_layout: &AudioIOLayout,
-        buffer_config: &BufferConfig,
+        _buffer_config: &BufferConfig,
         _context: &mut impl InitContext<Self>,
     ) -> bool {
-        // After `PEAK_METER_DECAY_MS` milliseconds of pure silence, the peak meter's value should
-        // have dropped by 12 dB
-        self.peak_meter_decay_weight = 0.25f64.powf((buffer_config.sample_rate as f64 * PEAK_METER_DECAY_MS / 1000.0).recip()) as f32;
-
         true
     }
 
     fn process(
         &mut self,
         buffer: &mut Buffer,
-        _aux: &mut AuxiliaryBuffers,
+        aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-
         //widgets::ParamEvent
         // Buffer level
-        for channel_samples in buffer.iter_samples() {
-            let mut in_amplitude = 0.0;
-            let num_samples = channel_samples.len();
-            //let scrollspeed = self.params.scrollspeed.value();
-            //let samples = &self.samples;
+        
+        // Reset this every buffer process
+        self.skip_counter = 0;
 
+        // Process the sidechain
+        for aux_channel_samples in aux.inputs[0].iter_samples() {
+            for aux_sample in aux_channel_samples {
+                // Apply gain
+                *aux_sample *= self.params.free_gain.smoothed.next();
+
+                // Only grab X samples to "optimize"
+                if self.skip_counter % self.params.h_scale.value() == 0 {
+                    // Update our samples vector for oscilloscope
+                    let mut aux_guard = self.aux_samples.lock();
+                    aux_guard.push(AtomicF32::new(*aux_sample));
+
+                    // Limit the size of the vector to X elements
+                    let scroll = self.params.scrollspeed.value() as usize;
+                    if aux_guard.len() > scroll {
+                        let trim_amount = aux_guard.len() - scroll;
+                        aux_guard.drain(0..=trim_amount);
+                    }
+                }
+                self.skip_counter += 1;
+            }
+        }
+
+        // Reset this every buffer process
+        self.skip_counter = 0;
+
+        for channel_samples in buffer.iter_samples() {
             for sample in channel_samples {
                 // Apply gain
                 *sample *= self.params.free_gain.smoothed.next();
                 
-                // Update the input meter amplitude
-                in_amplitude += *sample;
-                
-                // Update our samples vector for oscilloscope
-                let mut guard = self.samples.lock();
-                guard.push(AtomicF32::new(*sample));
+                // Only grab X samples to "optimize"
+                if self.skip_counter % self.params.h_scale.value() == 0 {
+                    // Update our samples vector for oscilloscope
+                    let mut guard = self.samples.lock();
+                    guard.push(AtomicF32::new(*sample));
 
-                
-                // Limit the size of the vector to 100 elements
-                if guard.len() > 100 {
-                    guard.remove(0);
+                    // Limit the size of the vector to X elements
+                    let scroll = self.params.scrollspeed.value() as usize;
+                    if guard.len() > scroll {
+                        let trim_amount = guard.len() - scroll;
+                        guard.drain(0..=trim_amount);
+                    }
                 }
-
+                self.skip_counter += 1;
             }
 
             // To save resources, a plugin can (and probably should!) only perform expensive
             // calculations that are only displayed on the GUI while the GUI is open
             if self.params.editor_state.is_open() {
-                // Input gain meter
-                in_amplitude = (in_amplitude / num_samples as f32).abs();
-                let current_peak_meter = self.peak_meter.load(std::sync::atomic::Ordering::Relaxed);
-                let new_peak_meter = if in_amplitude > current_peak_meter {
-                    in_amplitude
-                } else {
-                    current_peak_meter * self.peak_meter_decay_weight 
-                        + in_amplitude * (1.0 - self.peak_meter_decay_weight)
-                };
-
-                self.peak_meter.store(new_peak_meter, std::sync::atomic::Ordering::Relaxed);
+                // Do nothing
             }
         }
 
